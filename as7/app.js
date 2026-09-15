6657dee212860e13c5aaec0e293bb08e3cefd256
Aastik Madaan <madaanaastik@gmail.com>
2026-09-16 00:35:12 +0530

    Assignment-7 Uploaded

---
+++ as7/app.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+// Create the path of student.txt
+const filePath = path.join(__dirname, "data", "student.txt");
+
+// Read the file
+fs.readFile(filePath, "utf8", (err, data) => {
+    // Handle error
+    if (err) {
+        console.log("Error:", err.message);
+        return;
+    }
+
+    // Display student details
+    console.log("Student Details:");
+    console.log(data);
+
+    // Display file name
+    console.log("File Name:", path.basename(filePath));
+
+    // Display directory
+    console.log("Directory:", path.dirname(filePath));
+
+    // Display file extension
+    console.log("File Extension:", path.extname(filePath));
+});
